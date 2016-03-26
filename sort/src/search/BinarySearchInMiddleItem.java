d2387bc8ccb4061595c654e39b29a982a3a96187
konstantin8105 <konstantin8105@gmail.com>
2016-03-26 21:27:24 +0300

    first add BinarySearchInMiddleItem

---
+++ sort/src/search/BinarySearchInMiddleItem.java
@@ -0,0 +1,43 @@
+package search;
+
+public class BinarySearchInMiddleItem<T extends Comparable<T>> {
+
+    public int search(T[] list, T find) {
+        if (list == null)
+            throw new NullPointerException();
+        if (list.length == 0)
+            throw new IndexOutOfBoundsException();
+        if (list.length < 2)
+            return 0;
+
+        int low = 0;
+        int middle;
+        int high = list.length - 1;
+
+        while (low < high) {
+            middle = (high + low) >>> 1;
+
+            if (list[low].compareTo(find) == 0)
+                return low;
+            if (list[middle].compareTo(find) == 0)
+                return middle;
+            if (list[high].compareTo(find) == 0)
+                return high;
+
+            if (find.compareTo(list[low]) < 0 || find.compareTo(list[high]) > 0) {
+                throw new IndexOutOfBoundsException();
+            }
+
+            if(high - low == 1){
+                return low;
+            }
+
+            if (find.compareTo(list[middle]) > 0) {
+                low = middle;
+            } else {
+                high = middle;
+            }
+        }
+        throw new IndexOutOfBoundsException();
+    }
+}
