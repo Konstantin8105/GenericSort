a9c4a3008975c1f44f21f8b053481a4ddd7ce0c2
konstantin8105 <konstantin8105@gmail.com>
2016-04-01 23:27:06 +0300

    Start work on SquareSort

--- sort/src/sort/SquareSort.java
+++ sort/src/sort/SquareSort.java
@@ -1,5 +1,6 @@
 package sort;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SquareSort<T extends Comparable<T>> implements Sort<T> {
@@ -10,9 +11,23 @@ public class SquareSort<T extends Comparable<T>> implements Sort<T> {
         if (list.size() == 0)
             throw new IndexOutOfBoundsException();
 
-        int amountKeys = (int) Math.sqrt(list.size());
-
+        int amountKeys = (int) Math.max(Math.sqrt(list.size()), 1);
         System.out.println("amountKeys = " + amountKeys);
+
+        List<T> keys = new ArrayList<>();
+        for (int i = 0; i < amountKeys; i++) {
+            int positionInArray = i * amountKeys;
+            if (positionInArray < list.size()) {
+                keys.add(list.get(positionInArray));
+            }
+        }
+        System.out.println("keys = " + keys);
+
+        //sort
+
+        MergeSort<T> mergeSort = new MergeSort<>();
+        List<T> sortedKeys = mergeSort.sort(keys);
+        System.out.println("sortedKeys = " + sortedKeys);
 
 
         return null;
