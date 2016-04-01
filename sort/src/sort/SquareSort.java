bc50937f3ead006715e410fa8f30e5d8ec4fe63c
konstantin8105 <konstantin8105@gmail.com>
2016-04-01 23:23:27 +0300

    Start work on SquareSort

--- sort/src/sort/SquareSort.java
+++ sort/src/sort/SquareSort.java
@@ -1,4 +1,20 @@
 package sort;
 
-public class SquareSort {
+import java.util.List;
+
+public class SquareSort<T extends Comparable<T>> implements Sort<T> {
+    @Override
+    public List<T> sort(List<T> list) {
+        if (list == null)
+            throw new NullPointerException();
+        if (list.size() == 0)
+            throw new IndexOutOfBoundsException();
+
+        int amountKeys = (int) Math.sqrt(list.size());
+
+        System.out.println("amountKeys = " + amountKeys);
+
+
+        return null;
+    }
 }
